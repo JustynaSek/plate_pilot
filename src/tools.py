d5ed708aeed0fdf62c442239c1520a0276486690
Justyna Sek <justyna.sek@motorolasolutions.com>
2025-06-02 08:57:30 +0200

    switched to duckduck instead of searchapi

--- src/tools.py
+++ src/tools.py
@@ -1,25 +1,23 @@
-from langchain_community.utilities import SearchApiAPIWrapper
+# src/langchain_logic/core/tools.py
+
+# --- Imports ---
+# Remove: from langchain_community.utilities import SearchApiAPIWrapper
+from langchain_community.tools import DuckDuckGoSearchRun # New import for DuckDuckGo
 from langchain.agents import AgentExecutor, create_react_agent
 from langchain_core.tools import Tool
-from langchain_openai import ChatOpenAI
 import os
 from langchain import hub
-from llm_config import llm_agent
+from llm_config import llm_agent 
 
-searchapi_api_key = os.environ.get("SEARCHAPI_API_KEY")
-tools = []
-if searchapi_api_key:
-    search_tool = SearchApiAPIWrapper(searchapi_api_key=searchapi_api_key)
-    tools = [
-        Tool(
-            name="intermediate_answer",
-            func=search_tool.run,
-            description="useful for when you need to ask with search",
-        )
-    ]
-else:
-    tools = []
-    print("SerpAPI API key not found. Web search will be disabled.")
+duckduckgo_search = DuckDuckGoSearchRun()
+
+tools = [
+    Tool(
+        name="web_search",
+        func=duckduckgo_search.run,
+        description="Useful for when you need to answer questions about current events or perform general web searches, especially for recipes or dietary information. Input should be a concise query.",
+    )
+]
 
 prompt = hub.pull("hwchase17/react")
 agent = create_react_agent(llm_agent, tools, prompt)
